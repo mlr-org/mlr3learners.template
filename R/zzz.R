9b1ee6f5f0e4fabf234700202c4a0a7da977ca07
pat-s <patrick.schratz@gmail.com>
2020-03-31 22:06:29 +0200

    add lintr setup

--- R/zzz.R
+++ R/zzz.R
@@ -15,17 +15,17 @@ register_mlr3 = function(libname, pkgname) {
   # Example: x$add("regr.gamboost", LearnerRegrGAMBoost)
 }
 
-.onLoad = function(libname, pkgname) {
+.onLoad = function(libname, pkgname) { # nolint
   register_mlr3()
   setHook(packageEvent("mlr3", "onLoad"), function(...) register_mlr3(),
     action = "append")
 }
 
-.onUnload = function(libpath) {
+.onUnload = function(libpath) { # nolint
   event = packageEvent("mlr3", "onLoad")
   hooks = getHook(event)
   pkgname = vapply(hooks, function(x) environment(x)$pkgname, NA_character_)
-  setHook(event, hooks[pkgname != "mlr3learners.mboost"],
+  setHook(event, hooks[pkgname != "<package>"],
     action = "replace")
 }
 # nocov end
