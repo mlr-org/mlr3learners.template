c8088064bfea2c74a1cbb450f7b081a979d06cdc
pat-s <patrick.schratz@gmail.com>
2020-04-01 11:33:19 +0200

    update paramtest skeleton

--- tic.R
+++ tic.R
@@ -1,4 +1,10 @@
-# installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks()
+# R CMD check
+if (!ci_has_env("PARAMTEST")) {
+  do_package_checks()
 
-do_drat("mlr3learners/mlr3learners.drat")
+  do_drat("mlr3learners/mlr3learners.drat")
+} else {
+  # PARAMTEST
+  get_stage("install") %>%
+    add_step(step_install_deps())
+}
