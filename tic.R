4ce3dac3c379bcc522c0959dcf9cd455643367aa
pat-s <patrick.schratz@gmail.com>
2020-04-03 21:09:55 +0200

    comment out drat deployment initially to not fail CI during a learner being WIP

--- tic.R
+++ tic.R
@@ -2,7 +2,9 @@
 if (!ci_has_env("PARAMTEST")) {
   do_package_checks()
 
-  do_drat("mlr3learners/mlr3learners.drat")
+  # comment this line in once the learner has been approved
+  # this deploys to mlr3learners.drat
+  # do_drat("mlr3learners/mlr3learners.drat")
 } else {
   # PARAMTEST
   get_stage("install") %>%
